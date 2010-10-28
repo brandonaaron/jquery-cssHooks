c2eafa61e83294e3b38c3cab802565698a9f9dce
Burin Asavesna <burinasavesna@gmail.com>
2010-10-28 17:42:24 -0500

    testing actual return values against expected values

--- tests/test.js
+++ tests/test.js
@@ -1,11 +1,14 @@
 test("padding", function() {
     ok( jQuery("#test").css("padding"), "1px 2px 3px 4px" );
+    equals( jQuery("#test").css("padding"), "1px 2px 3px 4px", "returns values in the correct order" );
 });
 
 test("margin", function() {
     ok( jQuery("#test").css("margin"), "1px 2px 3px 4px" );
+    equals( jQuery("#test").css("margin"), "1px 2px 3px 4px", "returns values in the correct order" );
 });
 
 test("backgroundPosition", function() {
-    ok( jQuery("#test").css("backgroundPosition"), "5px 5px" );
+    ok( jQuery("#test").css("backgroundPosition"), "3px 5px" );
+    equals( jQuery("#test").css("backgroundPosition"), "3px 5px", "returns values in the correct order" );
 });
