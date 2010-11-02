d5c4eee2e87d0b116721dafbdd650002a2025a05
Brandon Aaron <brandon.aaron@gmail.com>
2010-11-02 08:41:00 -0500

    hfx integration for the  boxShadow cssHooks

--- boxshadow.js
+++ boxshadow.js
@@ -4,16 +4,18 @@
 (function($) {
     // boxShadow get hooks
     var div = document.createElement('div'),
-    divStyle = div.style,
-    support = $.support;
+        divStyle = div.style,
+        support = $.support;
 
-    support.boxShadow = 
-    divStyle.MozBoxShadow === ''? 'MozBoxShadow' :
-    (divStyle.MsBoxShadow === ''? 'MsBoxShadow' :
-    (divStyle.WebkitBoxShadow === ''? 'WebkitBoxShadow' : 
-    (divStyle.OBoxShadow === ''? 'OBoxShadow' :
-    (divStyle.BoxShadow === ''? 'BoxShadow' :
-    false))));
+    support.boxShadow =
+        divStyle.MozBoxShadow     === ''? 'MozBoxShadow'    :
+        (divStyle.MsBoxShadow     === ''? 'MsBoxShadow'     :
+        (divStyle.WebkitBoxShadow === ''? 'WebkitBoxShadow' :
+        (divStyle.OBoxShadow      === ''? 'OBoxShadow'      :
+        (divStyle.BoxShadow       === ''? 'BoxShadow'       :
+        false))));
+
+    div = null;
 
     // helper function to inject a value into an existing string
     // is there a better way to do this? it seems like a common pattern
@@ -23,7 +25,7 @@
         return parts.join(" ");
     }
 
-    if ($.support.boxShadow) {
+    if (support.boxShadow) {
         $.cssHooks.boxShadow = {
             get: function( elem, computed, extra ) {
                 return $.css(elem, support.boxShadow);
@@ -32,7 +34,7 @@
                 elem.style[ support.boxShadow ] = value;
             }
         };
-        
+
         $.cssHooks.boxShadowColor = {
             get: function ( elem, computed, extra ) {
                 return $.css(elem, support.boxShadow).split(/\)\s/)[0] + ')';
@@ -41,7 +43,7 @@
                 elem.style[ support.boxShadow ] = value + " " + $.css(elem, support.boxShadow).split(/\)\s/)[1];
             }
         };
-        
+
         $.cssHooks.boxShadowBlur = {
             get: function ( elem, computed, extra ) {
                 return $.css(elem, support.boxShadow).split(/\s/)[5];
@@ -50,7 +52,7 @@
                 elem.style[ support.boxShadow ] = insert_into($.css(elem, support.boxShadow), value, 5);
             }
         };
-        
+
         $.cssHooks.boxShadowSpread = {
             get: function ( elem, computed, extra ) {
                 return $.css(elem, support.boxShadow).split(/\s/)[6];
@@ -59,8 +61,7 @@
                 elem.style[ support.boxShadow ] = insert_into($.css(elem, support.boxShadow), value, 6);
             }
         };
-        
-        
+
         $.cssHooks.boxShadowX = {
             get: function ( elem, computed, extra ) {
                 return $.css(elem, support.boxShadow).split(/\s/)[3];
@@ -69,8 +70,7 @@
                 elem.style[ support.boxShadow ] = insert_into($.css(elem, support.boxShadow), value, 3);
             }
         };
-        
-        
+
         $.cssHooks.boxShadowY = {
             get: function ( elem, computed, extra ) {
                 return $.css(elem, support.boxShadow).split(/\s/)[4];
@@ -79,7 +79,15 @@
                 elem.style[ support.boxShadow ] = insert_into($.css(elem, support.boxShadow), value, 4);
             }
         };
-        
+
+        // setup fx hooks
+        var fxHooks = "Blur Spread X Y".split(" ");
+        $.each(fxHooks, function( i, suffix ) {
+            var hook = "boxShadow" + suffix;
+            $.fx.step[ hook ] = function( fx ) {
+                $.cssHooks[ hook ].set( fx.elem, fx.now + fx.unit );
+            };
+        });
     }
 
 })(jQuery);
--- tests/unit/boxshadow.js
+++ tests/unit/boxshadow.js
@@ -10,22 +10,42 @@ test("boxShadowColor", 2, function() {
     equals( jQuery("#test").css("boxShadowColor", "#ccc").css("boxShadow"), "rgb(204, 204, 204) 1px 2px 10px 0px", "sets the color properly" );
 });
 
-test("boxShadowBlur", 2, function() {
+test("boxShadowBlur", 3, function() {
     equals( jQuery("#test").css("boxShadowBlur"), "10px", "returns just the blur" );
     equals( jQuery("#test").css("boxShadowBlur", "42px").css("boxShadow"), "rgb(0, 0, 0) 1px 2px 42px 0px", "sets the blur properly" );
+    stop();
+    jQuery("#test").animate({ boxShadowBlur: 20 }, 100, function() {
+        equals( jQuery("#test").css("boxShadowBlur"), "20px", "animates the value properly" );
+        start();
+    });
 });
 
-test("boxShadowSpread", 2, function() {
+test("boxShadowSpread", 3, function() {
     equals( jQuery("#test").css("boxShadowSpread"), "0px", "returns just the spread" );
     equals( jQuery("#test").css("boxShadowSpread", "42px").css("boxShadow"), "rgb(0, 0, 0) 1px 2px 10px 42px", "sets the spread properly" );
+    stop();
+    jQuery("#test").animate({ boxShadowSpread: 20 }, 100, function() {
+        equals( jQuery("#test").css("boxShadowSpread"), "20px", "animates the value properly" );
+        start();
+    });
 });
 
-test("boxShadowX", 2, function() {
+test("boxShadowX", 3, function() {
     equals( jQuery("#test").css("boxShadowX"), "1px", "returns just the x offset" );
     equals( jQuery("#test").css("boxShadowX", "42px").css("boxShadow"), "rgb(0, 0, 0) 42px 2px 10px 0px", "sets the x offset properly" );
+    stop();
+    jQuery("#test").animate({ boxShadowX: 20 }, 100, function() {
+        equals( jQuery("#test").css("boxShadowX"), "20px", "animates the value properly" );
+        start();
+    });
 });
 
-test("boxShadowY", 2, function() {
+test("boxShadowY", 3, function() {
     equals( jQuery("#test").css("boxShadowY"), "2px", "returns just y offset" );
     equals( jQuery("#test").css("boxShadowY", "42px").css("boxShadow"), "rgb(0, 0, 0) 1px 42px 10px 0px", "sets the x offset properly" );
+    stop();
+    jQuery("#test").animate({ boxShadowY: 20 }, 100, function() {
+        equals( jQuery("#test").css("boxShadowY"), "20px", "animates the value properly" );
+        start();
+    });
 });
