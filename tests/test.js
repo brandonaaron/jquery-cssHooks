8a489c23ac1d3aa6e5484c3586ca89564a7ba278
Burin Asavesna <burinasavesna@gmail.com>
2010-11-01 20:35:08 -0500

    test for box shadow

--- tests/test.js
+++ tests/test.js
@@ -1,17 +1,17 @@
 test("margin", function() {
     equals( jQuery("#test").css("margin"), "1px 2px 3px 4px", "returns values in the correct order" );
-    equals( jQuery("#test").css("margin", "4px 3px 2px 1px").css("margin"), "4px 3px 2px 1px", "sets the values propery" );
-    equals( jQuery("#test").css("margin", "5px 6px 7px").css("margin"), "5px 6px 7px 6px", "sets the values properly" );
-    equals( jQuery("#test").css("margin", "8px 9px").css("margin"), "8px 9px 8px 9px", "sets the values properly" );
-    equals( jQuery("#test").css("margin", "1px").css("margin"), "1px 1px 1px 1px", "sets the values properly" );
+    equals( jQuery("#test").css("margin", "4px 3px 2px 1px").css("margin"), "4px 3px 2px 1px", "sets the top, right, bottom, left values properly" );
+    equals( jQuery("#test").css("margin", "5px 6px 7px").css("margin"), "5px 6px 7px 6px", "sets the top, right & left, bottom values properly" );
+    equals( jQuery("#test").css("margin", "8px 9px").css("margin"), "8px 9px 8px 9px", "sets the top & bottom, left & right values properly" );
+    equals( jQuery("#test").css("margin", "1px").css("margin"), "1px 1px 1px 1px", "sets all the values properly" );
 });
 
 test("padding", function() {
     equals( jQuery("#test").css("padding"), "1px 2px 3px 4px", "returns values in the correct order" );
-    equals( jQuery("#test").css("padding", "4px 3px 2px 1px").css("padding"), "4px 3px 2px 1px", "sets the values propery" );
-    equals( jQuery("#test").css("padding", "5px 6px 7px").css("padding"), "5px 6px 7px 6px", "sets the values properly" );
-    equals( jQuery("#test").css("padding", "8px 9px").css("padding"), "8px 9px 8px 9px", "sets the values properly" );
-    equals( jQuery("#test").css("padding", "1px").css("padding"), "1px 1px 1px 1px", "sets the values properly" );
+    equals( jQuery("#test").css("padding", "4px 3px 2px 1px").css("padding"), "4px 3px 2px 1px", "sets the top, right, bottom, left values properly" );
+    equals( jQuery("#test").css("padding", "5px 6px 7px").css("padding"), "5px 6px 7px 6px", "sets the top, right & left, bottom values properly" );
+    equals( jQuery("#test").css("padding", "8px 9px").css("padding"), "8px 9px 8px 9px", "sets the top & bottom, left & right values properly" );
+    equals( jQuery("#test").css("padding", "1px").css("padding"), "1px 1px 1px 1px", "sets all the values properly" );
 });
 
 test("backgroundPosition", function() {
@@ -37,4 +37,8 @@ test("backgroundPositionY", function() {
         equals( jQuery("#test").css("backgroundPositionY"), "10px", "animates the value properly" );
         start();
     });
+});
+
+test("boxShadow", function() {
+    equals( jQuery("#test").css("boxShadow"), "1px 1px 10px #000000", "returns values in the correct order (x offset, y offset, blur, color)" );
 });
