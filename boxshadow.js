1d8b502b7a2800754340a95e0bad1a5a827b20b9
Burin Asavesna <burinasavesna@gmail.com>
2010-11-01 21:02:05 -0500

    generic boxShadow without prefix?

--- boxshadow.js
+++ boxshadow.js
@@ -2,25 +2,25 @@
  * Licensed under the MIT License (LICENSE.txt).
  */
 (function($) {
-  // boxShadow get hooks
-  var div = document.createElement('div'),
-  divStyle = div.style,
-  support = $.support;
+    // boxShadow get hooks
+    var div = document.createElement('div'),
+    divStyle = div.style,
+    support = $.support;
 
-  support.boxShadow = 
-  divStyle.MozBoxShadow === ''? 'MozBoxShadow' :
-  (divStyle.MsBoxShadow === ''? 'MsBoxShadow' :
-  (divStyle.WebkitBoxShadow === ''? 'WebkitBoxShadow' : 
-  (divStyle.OBoxShadow === ''? 'OBoxShadow' :
-  false)));
+    support.boxShadow = 
+    divStyle.MozBoxShadow === ''? 'MozBoxShadow' :
+    (divStyle.MsBoxShadow === ''? 'MsBoxShadow' :
+    (divStyle.WebkitBoxShadow === ''? 'WebkitBoxShadow' : 
+    (divStyle.OBoxShadow === ''? 'OBoxShadow' :
+    (divStyle.BoxShadow === ''? 'BoxShadow' :
+    false))));
 
-  if ($.support.boxShadow) {
-    $.cssHooks.boxShadow = {
-      get: function( elem, computed, extra ) {
-        return $.css(elem, support.boxShadow);
-      }
-    };
-  }
-
+    if ($.support.boxShadow) {
+        $.cssHooks.boxShadow = {
+            get: function( elem, computed, extra ) {
+                return $.css(elem, support.boxShadow);
+            }
+        };
+    }
 
 })(jQuery);
