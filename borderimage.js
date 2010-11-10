e03333127888f2fd72d0c9c09871e2b18cbbc4f0
Tom Ellis <grohl_allmylife@hotmail.com>
2010-11-10 13:18:26 +0000

    initial border image csshook

---
+++ borderimage.js
@@ -0,0 +1,33 @@
+/*! Copyright (c) 2010 Tom Ellis (http://www.webmuse.co.uk)
+* Licensed under the MIT License (LICENSE.txt).
+*/
+(function($) {
+    // Border Image set and get hooks
+	var div = document.createElement( "div" );
+	
+    $.support.borderImage =
+    div.style.MozBorderImage === '' ? 'MozBorderImage' :
+    (div.style.WebkitBorderImage === '' ? 'WebkitBorderImage' :
+    (div.style.borderImage === '' ? 'borderImage' : false));
+
+	
+    if ( $.support.borderImage && $.support.borderImage !== "borderImage" && $.cssHooks )
+    {
+        $.cssHooks.borderImage = {
+        
+            get: function( elem, computed, extra ) {
+            
+                return $.css(elem, $.support.borderImage);
+            },
+            set: function( elem, value ) {
+            
+                elem.style[$.support.borderImage] = value;
+            }
+			
+        };
+		    
+    }
+        
+    div = null;
+    
+})(jQuery); 
