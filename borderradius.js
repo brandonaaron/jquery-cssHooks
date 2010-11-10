5de82d2802e890e2d1977700fc1e31b7acfb96e4
Burin Asavesna <burinasavesna@gmail.com>
2010-11-09 23:14:18 -0600

    adding prefix to the borderCornerRadius helper

--- borderradius.js
+++ borderradius.js
@@ -8,6 +8,7 @@
         support = $.support,
         dirs = "TopLeft TopRight BottomRight BottomLeft".split(" ");
 
+    // WebKit supports "borderRadius" as well as "WebKitBorderRadius", weird
     support.borderRadius =
         divStyle.MozBorderRadius     === ''? 'MozBorderRadius'    :
         (divStyle.MsBorderRadius     === ''? 'MsBorderRadius'     :
@@ -17,36 +18,40 @@
         false))));
 
     div = null;
-    
-    function borderCornerRadius(dir) {
+
+    function borderCornerRadius(direction, prefix) {
+        prefix = prefix === undefined || prefix === '' ? 'border' : prefix + 'Border';
         if ( support.borderRadius && support.borderRadius == "MozBorderRadius" ) {
-            return "MozBorderRadius" + "Topleft"; // e.g. MozBorderRadiusTopleft
+            // e.g. MozBorderRadiusTopleft
+            return prefix + "Radius" + direction.charAt(0).toUpperCase()+direction.substr(1).toLowerCase();
         } else {
-            return "border" + dir + "Radius";
+            // e.g. WebKitBorderTopLeftRadius, borderTopLeftRadius, etc
+            return prefix + direction + "Radius";
         }
     }
-    
+
     if ( support.borderRadius && support.borderRadius !== "BorderRadius" ) {
+        var vendor_prefix = support.borderRadius.replace('BorderRadius','');
         $.cssHooks.borderRadius = {
             get: function( elem, computed, extra ) {
+                // return each of the directions, topleft, topright, bottomright, bottomleft
                 return $.map(dirs, function( dir ) {
-                    return $.css(elem, borderCornerRadius( dir ));
+                    return $.css(elem, borderCornerRadius( dir, vendor_prefix ));
                 }).join(" ");
             },
             set: function( elem, value ) {
-                $.each( dirs, function( i, dir ) {
-                    elem.style[ borderCornerRadius( dir ) ] = value;
-                });
+                // takes in a single value or shorthand, e.g. 5px to set all, or 5px 0 0 5px to set left corners
+                elem.style[ borderCornerRadius( '', vendor_prefix ) ] = value;
             }
         };
 
         $.each(dirs, function( i, dir ) {
             $.cssHooks[ "borderRadius" + dir ] = {
                 get: function( elem, computed, extra ) {
-                    return $.css(elem, borderCornerRadius( dir ));
+                    return $.css(elem, borderCornerRadius( dir, vendor_prefix ));
                 },
                 set: function( elem, value ) {
-                    elem.style[ borderCornerRadius( dir ) ] = value;
+                    elem.style[ borderCornerRadius( dir, vendor_prefix ) ] = value;
                 }
             };
         });
