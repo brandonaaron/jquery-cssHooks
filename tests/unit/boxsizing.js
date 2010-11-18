00c137e5e12dcf27f7556c028cc77d9785ba5993
Tom Ellis <grohl_allmylife@hotmail.com>
2010-11-18 21:02:57 +0000

    added tests for box sizing

---
+++ tests/unit/boxsizing.js
@@ -0,0 +1,5 @@
+module("boxSizing");
+test("boxSizing", 2, function() {
+	equals( jQuery("#test").css("boxSizing"), "border-box", "returns correct value" );
+	equals( jQuery("#test").css("boxSizing","content-box").css("boxSizing"), "content-box", "sets and gets correct value" );
+});
