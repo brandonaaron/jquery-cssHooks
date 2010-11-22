365d56c55b4c1281acfd19bc039782971f0111e3
Tom Ellis <grohl_allmylife@hotmail.com>
2010-11-22 18:39:10 +0000

    initial multiple columns csshook

---
+++ columns.js
@@ -0,0 +1,46 @@
+/*! Copyright (c) 2010 Tom Ellis (http://www.webmuse.co.uk)
+* Licensed under the MIT License (license.txt).
+*/
+(function($) {
+    // Multiple Columns get hooks
+
+    var div = document.createElement("div");
+
+    var rWhitespace = /\s/,
+    column = "Column",
+    props = "Span Count Gap Width RuleColor RuleStyle RuleWidth".split(rWhitespace),
+    prefix = div.style.WebkitColumnGap === ''? 'Webkit' : (div.style.MozColumnGap === '' ? 'Moz' : '');
+    
+    $.support.columnCount =
+    div.style.WebkitColumnCount === ''? 'WebkitColumnCount' :
+    (div.style.MozColumnCount === ''? 'MozColumnCount' :
+    (div.style.columnCount === ''? 'columnCount' : false));
+
+    
+    function getCssProperty( prefix, prop )
+    {
+    	return prefix + ( (prefix === '') ? column.toLowerCase() : column ) + prop
+    }
+
+    if ( $.support.columnCount && $.support.columnCount !== "columnCount" )
+    {
+        $.each(props, function( i, prop ) {
+       
+           
+            $.cssHooks["column" + prop ] = {
+                get: function( elem, computed, extra ) {
+                    return $.css(elem, getCssProperty( prefix, prop ) );
+                },
+                set: function( elem, value ) {
+                    elem.style[ getCssProperty( prefix, prop ) ] = value;
+                }
+            };
+           
+        });
+
+
+    }
+   
+    div = null;
+   
+})(jQuery);
