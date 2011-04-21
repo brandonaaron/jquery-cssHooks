26dcff7aa304e780bd3bb61c249305c2b495e337
Tom Ellis <grohl_allmylife@hotmail.com>
2011-04-21 19:50:34 +0100

    Tidied up code and removed check for jQuery.cssHooks

--- borderimage.js
+++ borderimage.js
@@ -1,25 +1,21 @@
-/*! Copyright (c) 2010 Tom Ellis (http://www.webmuse.co.uk)
-* Licensed under the MIT License (LICENSE.txt).
+/*! 
+*	Copyright (c) 2011 Tom Ellis (http://www.webmuse.co.uk)
+*	User Select cssHook for jQuery 1.4.3+
+*	Licensed under the MIT License (LICENSE.txt).
 */
 (function($) {
     // Border Image set and get hooks
     
-    
-    if( !$.cssHooks )
-    {
-    	$.error( "jQuery 1.4.3+ is needed for this plugin to work" );
-    }
-    
-	var div = document.createElement( "div" );
-	
-    $.support.borderImage =
-    div.style.MozBorderImage === '' ? 'MozBorderImage' :
-    (div.style.WebkitBorderImage === '' ? 'WebkitBorderImage' :
-    (div.style.borderImage === '' ? 'borderImage' : false));
+	var div = document.createElement( "div" ),
+		divStyle = div.style,
+		$.support.borderImage =
+		div.style.MozBorderImage === '' ? 'MozBorderImage' :
+		(div.style.WebkitBorderImage === '' ? 'WebkitBorderImage' :
+		(div.style.borderImage === '' ? 'borderImage' : false));
 
 	
-    if ( $.support.borderImage && $.support.borderImage !== "borderImage" )
-    {
+    if ( $.support.borderImage && $.support.borderImage !== "borderImage" ){
+        
         $.cssHooks.borderImage = {
         
             get: function( elem, computed, extra ) {
@@ -35,6 +31,6 @@
 		    
     }
         
-    div = null;
+    div = divStyle = null;
     
 })(jQuery); 
