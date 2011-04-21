304dc8dbbeb4aef3bc6e422ccb3ce15f03c8c205
Tom Ellis <grohl_allmylife@hotmail.com>
2011-04-21 20:13:06 +0100

    Tidied up code, removed check for jQuery.cssHooks

--- boxsizing.js
+++ boxsizing.js
@@ -1,29 +1,23 @@
-/*! Copyright (c) 2010 Tom Ellis (http://www.webmuse.co.uk)
+/*! Copyright (c) 2011 Tom Ellis (http://www.webmuse.co.uk)
+* User Select cssHook for jQuery 1.4.3+
 * Licensed under the MIT License (LICENSE.txt).
 */
 (function($) {
 
     // Box Sizing set and get hooks
-
-    if( !$.cssHooks )
-    {
-    	$.error( "jQuery 1.4.3+ is needed for this plugin to work" );
-    	return;
-    }
 	
-	var div = document.createElement("div");
-	
-	$.support.boxSizing =
-    div.style.MozBoxSizing === ''? 'MozBoxSizing' : 
-	(div.style.WebkitBoxSizing === ''? 'WebkitBoxSizing' : 
-	(div.style.MsBoxSizing === ''? 'MsBoxSizing' :
-    (div.style.OBoxSizing === ''? 'OBoxSizing' : 
-	(div.style.ICabBoxSizing === ''? 'ICabBoxSizing' : 
-	(div.style.KhtmlBoxSizing === ''? 'KhtmlBoxSizing' : 
-	(div.style.boxSizing === ''? 'boxSizing' : false))))));
+	var div = document.createElement("div"),
+		divStyle = div.style,
+		$.support.boxSizing =
+			div.style.MozBoxSizing === ''? 'MozBoxSizing' : 
+			(div.style.WebkitBoxSizing === ''? 'WebkitBoxSizing' : 
+			(div.style.MsBoxSizing === ''? 'MsBoxSizing' :
+			(div.style.OBoxSizing === ''? 'OBoxSizing' : 
+			(div.style.ICabBoxSizing === ''? 'ICabBoxSizing' : 
+			(div.style.KhtmlBoxSizing === ''? 'KhtmlBoxSizing' : 
+			(div.style.boxSizing === ''? 'boxSizing' : false))))));
 
-	if ( $.support.boxSizing && $.support.boxSizing !== "boxSizing" )
-	{
+	if ( $.support.boxSizing && $.support.boxSizing !== "boxSizing" ){
 	
 		$.cssHooks.boxSizing = {
 			get: function( elem, computed, extra ) {
@@ -36,6 +30,6 @@
 			
 	}
 	
-	div = null;
+	div = divStyle = null;
 	
 })(jQuery);
