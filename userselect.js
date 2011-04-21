af438ea47840ffd281e584f5f00af4d6d1a86b90
Tom Ellis <grohl_allmylife@hotmail.com>
2011-04-21 20:06:22 +0100

    Tidied up code, removed check for jQuery.cssHooks and IE

--- userselect.js
+++ userselect.js
@@ -1,38 +1,18 @@
-/*! Copyright (c) 2010 Tom Ellis (http://www.webmuse.co.uk)
+/*! Copyright (c) 2011 Tom Ellis (http://www.webmuse.co.uk)
 * User Select cssHook for jQuery 1.4.3+
 * Licensed under the MIT License (LICENSE.txt).
 */
 (function($) {
 
 	//User Select cssHook
-	var div = document.createElement( "div" );
-	
-	/*
-	Values:	none | text | toggle | element | elements | all | inherit
-	Initial:	text
-	*/
-	
-    $.support.userSelect =
-    div.style.MozUserSelect === "" ? 'MozUserSelect' :
-    (div.style.WebkitUserSelect === "" ? 'WebkitUserSelect' :
-    (div.style.userSelect === ""  ? 'userSelect' : false));
-	
-
-	
-	var CssToJs = {
-	
-		"none" : "on",
-		"text" : "off",
-		"element" : "off",
-		"elements" : "off",
-		"all" : "off",
-		"inherit" : "off"
-	};
-
-	var isIE = (document.all) ? true : false; 
-		
-	if ( $.support.userSelect && $.support.userSelect !== "userSelect" && !isIE)
-	{
+	var div = document.createElement( "div" ),
+		divStyle = div.style,
+		$.support.userSelect =
+		div.style.MozUserSelect === "" ? 'MozUserSelect' :
+		(div.style.WebkitUserSelect === "" ? 'WebkitUserSelect' :
+		(div.style.userSelect === ""  ? 'userSelect' : false));
+			
+	if ( $.support.userSelect && $.support.userSelect !== "userSelect" ){
 		$.cssHooks.userSelect = {
 		
 			get: function( elem, computed, extra ) {
@@ -41,8 +21,7 @@
 			set: function( elem, value ) {
 				elem.style[ $.support.userSelect ] = value;
 			}
-		};	
-			
+		};		
 	}
 	
 	div = null;
