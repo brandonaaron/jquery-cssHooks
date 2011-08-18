043d1c81dfa56ba05dc0f5588f63f2b321632f8d
Tom Ellis <grohl_allmylife@hotmail.com>
2011-08-18 12:06:09 +0100

    Updated Border Image hook

--- borderimage.js
+++ borderimage.js
@@ -1,14 +1,13 @@
 /*! 
 * Copyright (c) 2011 Tom Ellis (http://www.webmuse.co.uk)
-* User Select cssHook for jQuery
+* Border Image cssHook for jQuery
 * Limitations:
   - Works with jQuery 1.4.3 and higher
   - Doesn't work in all CSS3 browers (currently)
 * Licensed under the MIT License (LICENSE.txt).
 */
 (function($) {
-    // Border Image set and get hooks
-    
+	
 	var div = document.createElement( "div" ),
 		divStyle = div.style;
     	$.support.borderImage =
@@ -21,14 +20,11 @@
         $.cssHooks.borderImage = {
         
             get: function( elem, computed, extra ) {
-            
                 return $.css(elem, $.support.borderImage);
             },
-            set: function( elem, value ) {
-            
+            set: function( elem, value ) {            
                 elem.style[$.support.borderImage] = value;
             }
-			
         };
     }
         
