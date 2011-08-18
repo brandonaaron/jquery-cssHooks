2085c4a15e251b1d3098e5fb79a3198b1fdeb883
Tom Ellis <grohl_allmylife@hotmail.com>
2011-08-18 12:06:09 +0100

    Updated Box Reflect hook

--- boxreflect.js
+++ boxreflect.js
@@ -1,34 +1,32 @@
 /*! 
 * Copyright (c) 2011 Tom Ellis (http://www.webmuse.co.uk)
-* User Select cssHook for jQuery
+* Box Reflect cssHook for jQuery
 * Limitations:
   - Works with jQuery 1.4.3 and higher
+  - Doesn't currently support gradients
 * Licensed under the MIT License (LICENSE.txt).
 */
 (function($) {
-    // Css3 Box Reflect set and get hooks 
 	var div = document.createElement('div'),
 		divStyle = div.style;
-		$.support.boxReflect =  divStyle.WebkitBoxReflect === '' ? 'WebkitBoxReflect' :
-			(divStyle.MozBoxReflect === '' ? 'MozBoxReflect' :
-			(divStyle.OBoxReflect === '' ? 'OBoxReflect' :
-			(divStyle.boxReflect === '' ? 'boxReflect' : false)));
-    
-	if ( $.support.boxReflect && $.support.boxReflect !== 'boxReflect' ){
 		
-		$.cssHooks.boxReflect = {
-			
+	$.support.boxReflect = 
+		divStyle.boxReflect === '' ? 'boxReflect' :
+		(divStyle.MozBoxReflect === '' ? 'MozBoxReflect' :
+		(divStyle.WebkitBoxReflect === '' ? 'WebkitBoxReflect' :
+		(divStyle.OBoxReflect === '' ? 'OBoxReflect' : false)));
+    
+	if ( $.support.boxReflect && $.support.boxReflect !== 'boxReflect' ) {
+		
+		$.cssHooks.boxReflect = {			
 			get: function( elem, computed, extra ) {
-				//Need to add support for gradients
 				return $.css( elem, $.support.boxReflect );
 			},
 			set: function( elem, value ) {
-				//Need to add support for gradients
 				elem.style[$.support.boxReflect] = value;
 			}
 		};	
 	}
-	
 	div = divStyle = null;
-	
+		
 })(jQuery);
