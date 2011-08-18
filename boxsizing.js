956de80c8bb4ccb57236dd041b37049daf9fb768
Tom Ellis <grohl_allmylife@hotmail.com>
2011-08-18 12:06:09 +0100

    Updated Box Sizing hook

--- boxsizing.js
+++ boxsizing.js
@@ -1,22 +1,21 @@
 /*! 
 * Copyright (c) 2011 Tom Ellis (http://www.webmuse.co.uk)
-* User Select cssHook for jQuery
+* Box Sizing cssHook for jQuery
 * Limitations:
   - Works with jQuery 1.4.3 and higher
 * Licensed under the MIT License (LICENSE.txt).
 */
 (function($) {
 
-    // Box Sizing set and get hooks
-	
 	var div = document.createElement("div"),
 		divStyle = div.style;
+		
 		$.support.boxSizing =
-			divStyle.MozBoxSizing === ''? 'MozBoxSizing' : 
-			(divStyle.WebkitBoxSizing === ''? 'WebkitBoxSizing' : 
-			(divStyle.MsBoxSizing === ''? 'msBoxSizing' :
-			(divStyle.boxSizing === ''? 'boxSizing' : false)));
-
+			divStyle.boxSizing === ''? 'boxSizing' : 
+			(divStyle.MozBoxSizing === ''? 'MozBoxSizing' : 
+			(divStyle.WebkitBoxSizing === ''? 'WebkitBoxSizing' :
+			(divStyle.MsBoxSizing === ''? 'msBoxSizing' : false)));
+					
 	if ( $.support.boxSizing && $.support.boxSizing !== "boxSizing" ){
 	
 		$.cssHooks.boxSizing = {
@@ -26,8 +25,7 @@
 			set: function( elem, value ) {
 				elem.style[ $.support.boxSizing ] = value;
 			}
-		};
-			
+		};		
 	}
 	
 	div = divStyle = null;
